39786cc31fee27c611907a1a99a90cd146c0a94d
scuevas1 <scuevas1@hawk.iit.edu>
2025-10-17 17:36:18 -0500

    adding in the elements for the filter and search. along with how many movies are on each page.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 (function () {
+  const cardsList = document.getElementById('cards');
+  const countEl = document.getElementById('count');
+  const filterForm = document.getElementById('filterForm');
+  const genreChips = document.getElementById('genreChips');
+  const yearSelect = document.getElementById('yearSelect');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  const pageOut = document.getElementById('pageOut');
 
+  
+  let allMovies = [];
+  let filteredMovies = [];
+  let currentPage = 1;
+  let perPage = 9;
 })();
